2e651a5c5158eb07c8cef602fd12a0f3f90eb22c
Seth Zyg <zygster11@gmail.com>
2021-01-27 14:35:32 -0600

    Pseudo-code for app somewhat written.

---
+++ Assets/js/index.js
@@ -0,0 +1,18 @@
+/*have prompts for employee input
+//capture these data and place them into an HTML document
+    //from scratch? have an index.html template?
+//email for employee has to actually open an email window
+//GitHub for employee makes a link to their profile
+
+
+//Start with:
+//On running app --> ask for team manager’s name, employee ID, email address, and office number
+    //once this has been passed in --> asks user if they want to add employees
+    //'with a menu with the option to add an engineer or an intern or to finish building my team'
+        --use the inquirer with options/choices for Engineer, Intern, or Finish
+    If engineer -->  engineer’s name, ID, email, and GitHub username
+        -->taken back to menu after input
+    If intern --> intern’s name, ID, email, and school
+        --> taken back to menu after input
+    If Finish --> writeFile('index.html', userData)
+    */
